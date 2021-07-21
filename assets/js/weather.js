2973084678a7aae20805d84dd16bc628cb902eb9
SC2021 <walterlaw19@gmail.com>
2021-07-21 00:55:13 -0400

    add function and fetch

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,5 +1,17 @@
 var getUserWeather = function() {
+    fetch("")
+    .then(function (response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayWeather(data.user);
+            })
+        }
+    })
     console.log("get weather");
+};
+
+var displayWeather = function() {
+
 };
 
 getUserWeather();

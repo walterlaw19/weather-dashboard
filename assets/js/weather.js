8e3563eb0ae3d6babbc5f05feca41d6d5c84a274
SC2021 <walterlaw19@gmail.com>
2021-07-31 01:50:21 -0400

    fix UV color and img

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,14 +2,12 @@
 var key = secret_api_key;
 var cityName = document.querySelector("#city-name"); // querySelector -> cna be used for id or classes or element tags
 console.log(cityName.value);
-var cityIdCounter = 0;
+
+var cityHistoryEl = document.querySelector("city-search-history");
+var counterCity = 0;
 
 // get user weather function
 var getUserWeather = function() {
-
-    
-    
-
 
     console.log("https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&units=imperial&appid=" + key)
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&units=imperial&appid=" + key)
@@ -18,6 +16,7 @@ var getUserWeather = function() {
     })
     .then(function(data) {
 
+        // moment() to get the current date
          function updateTime() {
             currentTime = moment().format('L');
             dateOne = moment().add(1, 'days').format('L');
@@ -25,60 +24,39 @@ var getUserWeather = function() {
             dateThree = moment().add(3, 'days').format('L');
             dateFour = moment().add(4, 'days').format('L');
             dateFive = moment().add(5, 'days').format('L');
-            // $("#currentDay").text(currentTime);
+          
         }
         updateTime()
 
+        // fetching City Name, Current date, weather img
+        var cityIconResultSpan = document.querySelector("#city-icon-result");
+        cityIconResultSpan.textContent = "";
 
-
-
-        var CityNameResultSpan = document.querySelector("#city-name-result");
-
-        var CityImg = data.weather[0].icon;
-        var cityimg2 = document.createElement("img");
-        console.log(CityImg);
-
-        var iconLink1 = "https://openweathermap.org/img/w/" + CityImg + ".png";
-        cityimg2.setAttribute('src', iconLink1);
-        CityNameResultSpan.appendChild(cityimg2);
-
-
-
-
-        // var jpgImg0 = document.createElement('img');
-        // var iconUrl0 = "https://openweathermap.org/img/w/" + data2.list[0].weather[0].icon + ".png";
-        // jpgImg0.setAttribute('src', iconUrl0);
-        //             iconTime0El.innerHTML = '';
-        //             iconTime0El.appendChild(jpgImg0);
-
+        var cityImg = data.weather[0].icon;
+        var cityImg2 = document.createElement("img");
         
 
+        var iconLink1 = "https://openweathermap.org/img/w/" + cityImg + ".png";
+        cityImg2.setAttribute('src', iconLink1);
+        cityIconResultSpan.appendChild(cityImg2);
 
-        CityNameResultSpan.textContent = data.name + " (" + (currentTime) + ")" + iconLink1;
+        var cityNameResultSpan = document.querySelector("#city-name-result");
+        cityNameResultSpan.textContent = data.name + " (" + (currentTime) + ")";
 
-
-        console.log(data)
-
+        // setting lon & lat to get information for 5 days forecast
         var lon = data.coord.lon;
         var lat = data.coord.lat;
 
+
+        // fetching 2nd time to obtain 5 day forecast using lon and lat
         fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + key)
         .then(function (response) {
             return response.json();
         })
         .then(function(data2) {
-            
 
-            
-            console.log(data2)
-
-            
-
-
-            // queryselect the element
+            // fetching temp, humi, wind, UV
             var tempSpan = document.querySelector("#temperature");
-        
-            // change the text content of the element
             tempSpan.textContent = "Temperature: " + Math.trunc(data2.current.temp) + " ℉";
 
             var humiSpan = document.querySelector("#humidity");
@@ -87,78 +65,71 @@ var getUserWeather = function() {
             var windSpeedSpan = document.querySelector("#wind-speed");
             windSpeedSpan.textContent = "Wind Speed: " + data2.current.wind_speed + " MPH";
 
-            // var UVI = data2.current.uvi;
             
-            // if (UVI < 3 ) {
-            //     uvIndexSpan.style.backgroundColor= "rgb(97, 211, 240)";
-            // } else if (UVI >=3 & UVI <=6) {
-            //     uvIndexSpan.style.backgroundColor= "rgb(218, 231, 31)";
-            // } else {
-            //     uvIndexSpan.style.backgroundColor= "rgb(245, 8, 8)";
-            // }
-
-
-
             var uvIndexSpan = document.querySelector("#uv-index");
-
-
-
             uvIndexSpan.textContent = "UV Index: " + data2.current.uvi;
 
+            // setting color alert base on UV index 
 
-
-            // forecast of 5 days --------------------------------------------
+            var uvControl = data2.current.uvi;
             
+            if (uvControl < 3) {
+                uvIndexSpan.style.backgroundColor = "green", uvIndexSpan.style.color = "white";
+            } else if (uvControl >=3 & uvControl <=6) {
+                uvIndexSpan.style.backgroundColor = "yellow", uvIndexSpan.style.color = "white";
+            } else {
+                uvIndexSpan.style.backgroundColor = "red", uvIndexSpan.style.color = "white";
+            }
+
+
+            // Starting Forecast for next 5 days ----------------------------->
+
 
             // forecast day 1
             var forecastOneDivEl = document.querySelector("#forecast-1");
             forecastOneDivEl.textContent = "";
             var forecastOneDateEl = document.createElement("p");
-    // var image create
+            var forecastOneIcon = document.createElement("img")
             var forecastOneTempEl = document.createElement("p");
             var forecastOneWindEl = document.createElement("p");
             var forecastOneHumidEl = document.createElement("p");
 
-
             forecastOneDateEl.textContent = dateOne;
-// image obtain
+            var forecastOneImg = data2.daily[0].weather[0].icon;
+            var forecastOneLink = "https://openweathermap.org/img/w/" + forecastOneImg + ".png";
+            forecastOneIcon.setAttribute("src", forecastOneLink);
             forecastOneTempEl.textContent = "temp: " + Math.trunc(data2.daily[0].temp.max) + " ℉";
             forecastOneWindEl.textContent = "Wind Speed: " + data2.daily[0].wind_speed + " MPH";
             forecastOneHumidEl.textContent = "Humidity: " + data2.daily[0].humidity + "%";
 
             console.log(forecastOneTempEl);
             forecastOneDivEl.appendChild(forecastOneDateEl);
-// append imag
+            forecastOneDivEl.appendChild(forecastOneIcon);
             forecastOneDivEl.appendChild(forecastOneTempEl);
             forecastOneDivEl.appendChild(forecastOneWindEl);
             forecastOneDivEl.appendChild(forecastOneHumidEl);
 
 
-             
-
-           
-
-            
- 
-
             // forecast day 2
             var forecastTwoDivEl = document.querySelector("#forecast-2");
             forecastTwoDivEl.textContent = "";
             var forecastTwoDateEl =  document.createElement("p");
-            // create var image
+            var forecastTwoIcon = document.createElement("img")
             var forecastTwoTempEl = document.createElement("p");
             var forecastTwoWindEl = document.createElement("p");
             var forecastTwoHumidEl = document.createElement("p");
 
             forecastTwoDateEl.textContent = dateTwo;
-            // obtain image
+            var forecastTwoImg = data2.daily[1].weather[0].icon;
+            var forecastTwoLink = "https://openweathermap.org/img/w/" + forecastTwoImg + ".png";
+            forecastTwoIcon.setAttribute("src", forecastTwoLink);
             forecastTwoTempEl.textContent = "temp: " + Math.trunc(data2.daily[1].temp.max) + " ℉";
             forecastTwoWindEl.textContent = "Wind Speed: " + data2.daily[1].wind_speed + " MPH";
             forecastTwoHumidEl.textContent = "Humidity: " + data2.daily[1].humidity + "%";
 
             console.log(forecastTwoTempEl);
             forecastTwoDivEl.appendChild(forecastTwoDateEl);
-            // append imag
+            forecastTwoDivEl.appendChild(forecastTwoIcon);
             forecastTwoDivEl.appendChild(forecastTwoTempEl);
             forecastTwoDivEl.appendChild(forecastTwoWindEl);
             forecastTwoDivEl.appendChild(forecastTwoHumidEl);
@@ -168,114 +139,93 @@ var getUserWeather = function() {
             var forecastThreeDivEl = document.querySelector("#forecast-3");
             forecastThreeDivEl.textContent = "";
             var forecastThreeDateEl =  document.createElement("p");
-            // create var image
+            var forecastThreeIcon = document.createElement("img");
             var forecastThreeTempEl = document.createElement("p");
             var forecastThreeWindEl = document.createElement("p");
             var forecastThreeHumidEl = document.createElement("p");
 
             forecastThreeDateEl.textContent = dateThree;
-            // obtain image
+            var forecastThreeImg = data2.daily[2].weather[0].icon;
+            var forecastThreeLink = "https://openweathermap.org/img/w/" + forecastThreeImg + ".png";
+            forecastThreeIcon.setAttribute("src", forecastThreeLink);
             forecastThreeTempEl.textContent = "temp: " + Math.trunc(data2.daily[2].temp.max) + " ℉";
             forecastThreeWindEl.textContent = "Wind Speed: " + data2.daily[2].wind_speed + " MPH";
             forecastThreeHumidEl.textContent = "Humidity: " + data2.daily[2].humidity + "%";
 
             console.log(forecastThreeTempEl);
             forecastThreeDivEl.appendChild(forecastThreeDateEl);
-            // append imag
+            forecastThreeDivEl.appendChild(forecastThreeIcon);
             forecastThreeDivEl.appendChild(forecastThreeTempEl);
             forecastThreeDivEl.appendChild(forecastThreeWindEl);
             forecastThreeDivEl.appendChild(forecastThreeHumidEl);
-
-
-
-
 
 
             // forecast day 4
             var forecastFourDivEl = document.querySelector("#forecast-4");
             forecastFourDivEl.textContent = "";
             var forecastFourDateEl =  document.createElement("p");
-            // create var image
+            var forecastFourIcon = document.createElement("img");
             var forecastFourTempEl = document.createElement("p");
             var forecastFourWindEl = document.createElement("p");
             var forecastFourHumidEl = document.createElement("p");
 
             forecastFourDateEl.textContent = dateFour;
-            // obtain image
+            var forecastFourImg = data2.daily[3].weather[0].icon;
+            var forecastFourLink = "https://openweathermap.org/img/w/" + forecastFourImg + ".png";
+            forecastFourIcon.setAttribute("src", forecastFourLink);
             forecastFourTempEl.textContent = "temp: " + Math.trunc(data2.daily[3].temp.max) + " ℉";
             forecastFourWindEl.textContent = "Wind Speed: " + data2.daily[3].wind_speed + " MPH";
             forecastFourHumidEl.textContent = "Humidity: " + data2.daily[3].humidity + "%";
 
             console.log(forecastFourTempEl);
             forecastFourDivEl.appendChild(forecastFourDateEl);
-            // append imag
+            forecastFourDivEl.appendChild(forecastFourIcon);
             forecastFourDivEl.appendChild(forecastFourTempEl);
             forecastFourDivEl.appendChild(forecastFourWindEl);
             forecastFourDivEl.appendChild(forecastFourHumidEl);
 
 
-
             // forecast day 5
             var forecastFiveDivEl = document.querySelector("#forecast-5");
             forecastFiveDivEl.textContent = "";
-            forecastFiveDivEl.style.display = "block";
             var forecastFiveDateEl =  document.createElement("p");
-            // create var image
+            var forecastFiveIcon = document.createElement("img");
             var forecastFiveTempEl = document.createElement("p");
             var forecastFiveWindEl = document.createElement("p");
             var forecastFiveHumidEl = document.createElement("p");
 
             forecastFiveDateEl.textContent = dateFive;
-            // obtain image
+            var forecastFiveImg = data2.daily[4].weather[0].icon;
+            var forecastFiveLink = "https://openweathermap.org/img/w/" + forecastFiveImg + ".png";
+            forecastFiveIcon.setAttribute("src", forecastFiveLink);
             forecastFiveTempEl.textContent = "temp: " + Math.trunc(data2.daily[4].temp.max) + " ℉";
             forecastFiveWindEl.textContent = "Wind Speed: " + data2.daily[4].wind_speed + " MPH";
             forecastFiveHumidEl.textContent = "Humidity: " + data2.daily[4].humidity + "%";
 
             console.log(forecastFiveTempEl);
             forecastFiveDivEl.appendChild(forecastFiveDateEl);
-            // append imag
+            forecastFiveDivEl.appendChild(forecastFiveIcon);
             forecastFiveDivEl.appendChild(forecastFiveTempEl);
             forecastFiveDivEl.appendChild(forecastFiveWindEl);
             forecastFiveDivEl.appendChild(forecastFiveHumidEl);
 
             cityName.value = "";
 
-            
-
-
-
-            // storage history
-
-        
-
+            // Storing cityname in local Storage
 
             localStorage.setItem(JSON.stringify("CityName"), data.name);
             console.log(JSON.stringify("CityName"), data.name);
 
-            var cityHistoryEl = document.querySelector("#city-search-history");
-
-
-
-            var cityHistorylistEl = document.createElement("li");
-            cityHistorylistEl.className = "cities";
-            cityHistorylistEl.setAttribute("city-id", cityIdCounter);
-            cityHistorylistEl.textContent = cityName.value;
-            cityIdCounter++
-            cityHistoryEl.appendChild(cityHistorylistEl);
-
-            
-   
-            
-            
+            // var cityLiEl = document.createElement("li");
+            // cityLiEl.className = "cities";
+            // cityLiEl.setAttribute("city-id", counterCity);
+            // cityLiEl.innerHTML = data.name;
+            // counterCity++;
+            // cityHistoryEl.appendChild(cityLiEl);
+ 
         })
-        
-
     });
-
 };
 
-
-
 var weatherButton = document.getElementById("btn-weather");
-
 weatherButton.addEventListener("click", getUserWeather);
